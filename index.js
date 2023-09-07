a679a980d79a7656879235531b9e0f1d1bbd3289
Jane Park <jane.sypark@gmail.com>
2023-09-06 21:45:10 -0400

    numbers

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random()+1);
+
+let num3 = 14;
+let num4 = 5;
+let remainder = 4;
+let mod = num3%num4;
+console.log(mod)
+
+let max = Math.max(5,6,7,8,20);
